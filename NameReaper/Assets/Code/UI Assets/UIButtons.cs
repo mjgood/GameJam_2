0230db8b6a8cb635cbec6661d407f056a715b2b5
jpgo5000 <jpgo5000@gmail.com>
2015-10-24 00:36:25 -0400

    UI Controls
    preliminary menu.  this took me 5 hours!  :)

---
+++ NameReaper/Assets/Code/UI Assets/UIButtons.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButtons : MonoBehaviour {
+    GameObject MMParent, SParent;
+	// Use this for initialization
+	void Start () {
+        MMParent = GameObject.Find("MainMenuParent");
+        SParent = GameObject.Find("SettingsParent");
+        SParent.transform.Translate(5000f, 5000f, -10f);
+    }
+	
+    public void playBTN()
+    {
+        Debug.Log("play!");
+    }
+    public void settingsBTN()
+    {
+        Debug.Log("settings");
+        SParent.transform.Translate(MMParent.transform.position.x-SParent.transform.position.x, MMParent.transform.position.y - SParent.transform.position.y, -10f);
+        MMParent.transform.Translate(5000f,5000f,-10f);
+    }
+    public void exitBTN()
+    {
+        Debug.Log("exit");
+    }
+    public void sound_FX()
+    {
+        Debug.Log("toggle sound effects");
+    }
+    public void music()
+    {
+        Debug.Log("toggle music");
+    }
+    public void back()
+    {
+        Debug.Log("return to menu");
+        MMParent.transform.Translate(SParent.transform.position.x-MMParent.transform.position.x, SParent.transform.position.y-MMParent.transform.position.y, -10f);
+        SParent.transform.Translate(5000f, 5000f, -10f);
+    }
+}
