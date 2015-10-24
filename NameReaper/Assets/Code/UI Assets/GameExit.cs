923e9bddd3050451f0ce640dfbf0fb32b2fb2628
jpgo5000 <jpgo5000@gmail.com>
2015-10-23 22:16:24 -0400

    main menu
    buttons added.  doesn't do anything yet

---
+++ NameReaper/Assets/Code/UI Assets/GameExit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameExit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+
+    void fixedUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Debug.Log("mouse released");
+        }
+    }
+
+    public void OnClick()
+    {
+        //Application.Quit();
+        
+    }
+}
